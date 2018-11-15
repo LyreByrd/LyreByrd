bbc13a4b1ada77795e655c0bed057ee9cfa18d42
Dominique Robinson <domrobinson.dj@gmail.com>
2018-11-15 11:06:10 -0600

    feature/add-styled-jsx-env

--- server/Routes/auth-routes.js
+++ server/Routes/auth-routes.js
@@ -3,13 +3,14 @@ const router = require('express').Router();
 router.post('/login', (req, res) => {
       console.log(req.body);
       res.end(JSON.stringify('login'));
-      // app.render(req, res, '/feed', req.query); 
+      // app.render(req, res, '/feed', req.query);
     });
 
 router.post('/signup', (req, res) => {
       console.log(req.body);
       res.end(JSON.stringify('signup'));
-      // app.render(req, res, '/feed', req.query); 
+      // app.render(req, res, '/feed', req.query);
     });
 
 module.exports = router;
+
