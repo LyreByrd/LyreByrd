8917256f4b480e4a9b3efbf87146354f4e6af020
Dominique Robinson <domrobinson.dj@gmail.com>
2018-11-17 17:09:14 -0600

    skeleton of main feed

---
+++ pages/components/block.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Card from './card';
+
+const Block = ({ title, subtitle, host, name }) => (
+  <div>
+    <div className="titlebox">
+      <p className="title">What's Streaming Now</p>
+      <p className="subtitle">Suggestions based on sucking at life right now</p>
+      <div className="gallery">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+    <style jsx>{`
+      .titlebox {
+        height: 300px;
+        border-bottom: 1px solid #dfdcd4;
+      }
+
+      .title {
+        font-size: 23px;
+        font-weight: 100;
+        line-height: 29.9px;
+        color: black;
+        text-decoration: none solid rgb(42, 45, 40);
+      }
+
+      .subtitle {
+        font-size: 14px;
+        font-weight: 100;
+        line-height: 19.9px;
+        color: #908c83;
+      }
+
+      .gallery {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        height: 200px;
+      }
+    `}</style>
+  </div>
+);
+
+export default Block;
--- pages/feed.js
+++ pages/feed.js
@@ -1,57 +1,97 @@
 import NavBar from './components/NavBar.js';
 import react from 'react';
 import axios from 'axios';
+import Block from './components/block';
 
-class Feed extends React.Component {
+export default class Feed extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      feeds: []
-    }
-
+      feeds: [],
+    };
   }
 
   componentDidMount() {
-    this.getFeeds()
+    this.getFeeds();
   }
 
   getFeeds() {
-    axios.get('/playerFeeds')
-    .then(feeds => {
-      let newFeeds = [];
-      feeds.data.forEach(feed => {
-        newFeeds.push([feed.host, feed.path])
-      })
-      if (this.state.feeds.length === 0) {
-        this.setState({
-          feeds: newFeeds
-        })
-      } else {
-        this.setState(prevState => {
-          feeds: [...prevState.feeds, ...newFeeds]
-        })
-      }
-    })
-    .catch(err => {
-      console.log('err getting feeds from db', err)
-    })
+    // axios
+    //   .get('/playerFeeds')
+    //   .then(feeds => {
+    //     let newFeeds = [];
+    //     feeds.data.forEach(feed => {
+    //       newFeeds.push([feed.host, feed.path]);
+    //     });
+    //     if (this.state.feeds.length === 0) {
+    //       this.setState({
+    //         feeds: newFeeds,
+    //       });
+    //     } else {
+    //       this.setState(prevState => {
+    //         feeds: [...prevState.feeds, ...newFeeds];
+    //       });
+    //     }
+    //   })
+    //   .catch(err => {
+    //     console.log('err getting feeds from db', err);
+    //   });
   }
 
   render() {
     return (
-      <div>
+      <div className="body">
         <header>
           <NavBar />
         </header>
-        <h1>Main Feed</h1>
-        <div>
-          {this.state.feeds.map((feed, i) => {
-            return <div key={i}>host: {feed[0]} path:{feed[1]}</div>
-          })}
+        <div className="container">
+          <div className="feed heading popular">
+            <Block />
+            <Block />
+            <Block />
+            <Block />
+            <Block />
+            <Block />
+
+            {this.state.feeds.map((feed, i) => {
+              return (
+                <div key={i}>
+                  host: {feed[0]} path:{feed[1]}
+                </div>
+              );
+            })}
+          </div>
+          <div className="sidebar" />
         </div>
+        <style jsx>{`
+          .body {
+            background-color: #dfdcd4;
+          }
+
+          .container {
+            display: grid;
+            grid-template-columns: 8fr 2fr;
+            background-color: white;
+            height: 2000.75px;
+            margin: 0 100px;
+            padding: 46px 30px 0 30px;
+          }
+
+          .feed {
+            display: grid;
+            grid-row-start: 1;
+            grid-auto-rows: 327px;
+            width: 100%;
+
+            padding: 10px;
+            grid-gap 5px;
+          }
+
+          .sidebar {
+            border-left: 1px solid #dfdcd4;
+          }
+        `}</style>
       </div>
-    )
+    );
   }
 }
-
-export default Feed;
