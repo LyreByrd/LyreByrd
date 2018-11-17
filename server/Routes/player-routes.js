d3fd0820cbbc9c10d3d6326faf2cae14ca7d312f
LNBertolini <lunbertolini@gmail.com>
2018-11-17 14:11:22 -0600

    Server endpoints for player fetching

---
+++ server/Routes/player-routes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+const syncServerUrl = process.env.SYNC_SERVER_URL || 'localhost';
+const syncServerPort = process.env.SYNC_SERVER_PORT || 3000;
+
+router.get('/host', (req, res) => {
+  axios.get(`http://${syncServerUrl}:${syncServerPort}/api/player/host`)
+    .then(response => {
+      res.setStatus(200).send(response.body);
+    })
+    .catch(err => {
+      if(err.status) {
+        res.setStatus(err.status).send(err);
+      } else {
+        res.sendStatus(500);
+      }
+    });
+})
+
+router.get('/client', (req, res) => {
+  axios.get(`http://${syncServerUrl}:${syncServerPort}/api/player/client`)
+    .then(response => {
+      res.setStatus(200).send(response.body);
+    })
+    .catch(err => {
+      if(err.status) {
+        res.setStatus(err.status).send(err);
+      } else {
+        res.sendStatus(500);
+      }
+    });
+})
+
+module.exports = router;
