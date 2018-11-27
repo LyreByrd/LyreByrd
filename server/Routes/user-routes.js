e4d54e1014fd56e96ab30469959f087c1dd0f0a0
Steven Duncan <steven.k.duncan@gmail.com>
2018-11-27 16:36:56 -0600

    rendering avatar from db

--- server/Routes/user-routes.js
+++ server/Routes/user-routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const passport = require('passport');
 const multer = require('multer');
-var fs = require('fs');
-var storage = multer.diskStorage({
+const fs = require('fs');
+
+const storage = multer.diskStorage({
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now())
   }
@@ -33,7 +34,7 @@ router.get('/spotify/redirect', passport.authenticate('spotify'), (req, res) => 
 
 router.post('/profile/avatar/upload', upload.single('avatarFile'), (req, res) => {
 
-  // console.log('req :', req.file);
+  // console.log('req.file.path :', req.file.path);
   // console.log('req.body :', req.body);
 
   const avatar = {};
@@ -55,8 +56,14 @@ router.post('/profile/avatar/upload', upload.single('avatarFile'), (req, res) =>
   } )
 });
 
-router.get('/user/profile/avatar', (req, res) => {
-  console.log('avatar requested')
+router.get('/profile/avatar', (req, res) => {
+  let username = req.query.username;
+  User.findOne({ username }, 'avatar', (err, result) => {
+    if (err) console.log('err getting avatar from db :', err);
+    else {
+      res.send(result.avatar.data);
+    }
+  })
 })
 
 
