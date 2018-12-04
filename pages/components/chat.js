5b8ab7744f775f37d76231c647f486962395d38e
Guillermo Maycotte <31870337+gmayc@users.noreply.github.com>
2018-12-03 19:58:04 -0600

    https update:wq

--- pages/components/chat.js
+++ pages/components/chat.js
@@ -75,8 +75,8 @@ class Chat extends react.Component {
 
   socketConnect() {
     //socket.io connection
-    const socket = io('http://18.188.27.120:8000'); //todo change to production.env host
-    const feedSocket = io('http://18.188.27.120:8080');
+    const socket = io('https://18.188.27.120:8000'); //todo change to production.env host
+    const feedSocket = io('https://18.188.27.120:8080');
     
     //on user connect
     socket.on('connect', () => {
